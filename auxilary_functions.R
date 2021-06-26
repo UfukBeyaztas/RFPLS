06cced0da9f81b15c5ff19a2e86bc4aaf3f36502
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2021-06-26 21:34:02 +0300

    Update auxilary_functions.R

--- auxilary_functions.R
+++ auxilary_functions.R
@@ -12,20 +12,20 @@ library(regRSM)
 options(warn = -1)
 
 #_____________________________________________________________________________________________________________________________
-#___________________________________________________________Design_matrix_____________________________________________________
+#_______________________________________________________Design_matrix_(FPLS)__________________________________________________
 #_____________________________________________________________________________________________________________________________
 
-getAmat = function(X_tr, X_te, nbf){
+getAmat = function(X_tr, X_te, nbf, rangeval){
   np = length(X_tr)
   
   dtp = list()
   for(i in 1:np)
-    dtp[[i]] = seq(0, 1, length=ncol(X_tr[[i]]))
+    dtp[[i]] = seq(rangeval[[i]][1], rangeval[[i]][2], length=ncol(X_tr[[i]]))
   
   Bsp_b = list()
   Bsp_f = list()
   for(i in 1:np){
-    Bsp_b[[i]] = create.bspline.basis(c(0,1), nbasis = nbf[i])
+    Bsp_b[[i]] = create.bspline.basis(c(rangeval[[i]][1], rangeval[[i]][2]), nbasis = nbf[i])
     Bsp_f[[i]] = eval.basis(dtp[[i]], Bsp_b[[i]])
   }
   
@@ -67,16 +67,16 @@ getAmat = function(X_tr, X_te, nbf){
 #_____________________________________________________________________________________________________________________________
 
 
-#_______________________________________________________________________________________________________________________________________________________________
-#_____________________________________________________________________FPLS_function_____________________________________________________________________________
-#_______________________________________________________________________________________________________________________________________________________________
+#_________________________________________________________________________________________________________________________________________________________________________
+#______________________________________________________________________________FPLS_function______________________________________________________________________________
+#_________________________________________________________________________________________________________________________________________________________________________
 fpls_fun = function(Y, X_tr, X_te, npls, method = c("classical", "robust")){
   np = length(X_tr)
   N = dim(X_tr[[1]])[1]
   M = numeric()
   for(im in 1:np)
     M[im] = dim(X_tr[[im]])[2]
-  
+
   pred_vals = list()
   fit_vals = list()
   coef_mat = list()
@@ -143,15 +143,15 @@ fpls_fun = function(Y, X_tr, X_te, npls, method = c("classical", "robust")){
   opt_preds = pred_vals[[npls]]
   opt_coefs = coef_mat[[npls]]
   
-  return(list(all_preds = pred_vals, fits_opt = opt_fits, preds_opt = opt_preds, coefs = opt_coefs, plscomps = comps))
+  return(list(all_fits = fit_vals, fits_opt = opt_fits, preds_opt = opt_preds, coefs = opt_coefs, plscomps = comps))
 }
 
-#_______________________________________________________________________________________________________________________________________________________________
-#_______________________________________________________________________________________________________________________________________________________________
+#_________________________________________________________________________________________________________________________________________________________________________
+#_________________________________________________________________________________________________________________________________________________________________________
 
 
 #___________________________________________________________________________________________________
-#__________________________________Estimated_coefficient_functions__________________________________
+#_______________________________Estimated_coefficient_functions_(PLS)_______________________________
 #___________________________________________________________________________________________________
 
 getCoef = function(object, plsComps, coefs){
@@ -161,9 +161,7 @@ getCoef = function(object, plsComps, coefs){
   Inn_prd_sqrts = object$InnProds
   np = length(Amats)
   npls = dim(plsComps[[1]])[2]
-  
-  for(ij in 1:np)
-    Amats[[ij]] = scale(Amats[[ij]])
+
   
   B0 = coefs[1]
   Brest = matrix(coefs[-1], ncol = npls, byrow = TRUE)
@@ -188,19 +186,130 @@ getCoef = function(object, plsComps, coefs){
 
 
 #__________________________________________________________________________________________
+#_________________________FPCA_components_and_scores_for_regression________________________
+#__________________________________________________________________________________________
+
+getPCA = function(X_tr, X_te, nbasis, ncomp, rangeval){
+  data = rbind(X_tr, X_te)
+  n_train = dim(X_tr)[1]
+  n_test = dim(X_te)[1]
+  n = dim(data)[1]
+  p = dim(data)[2]
+  dimnames(data)=list(as.character(1:n), as.character(1:p))
+  grid_points = seq(rangeval[1], rangeval[2], length.out = p)
+  bs_basis = create.bspline.basis(rangeval, nbasis = nbasis)
+  evalbase = eval.basis(grid_points, bs_basis)
+  fdobj = fdPar(bs_basis, int2Lfd(2), lambda=0)
+  pcaobj = smooth.basisPar(grid_points, t(data), bs_basis, Lfdobj=NULL, lambda=0)$fd
+  dpca = pca.fd(pcaobj[1:n_train,], nharm = ncomp, fdobj)
+  PCAscore = dpca$scores
+  PCAcoef = dpca$harmonics$coefs
+  fdXnew = pcaobj[-(1:n_train),]
+  fdXnew$coefs = t(scale(t(fdXnew$coefs), scale = F))
+  PCAscore_test = inprod(fdXnew, dpca$harmonics)
+  return(list(PCAcoef = PCAcoef, PCAscore = PCAscore, PCAscore_test = PCAscore_test, evalbase = evalbase))
+}
+
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+#__________________________________________________________________________________________
+#_________________________________FPCA_parameter_estimation________________________________
+#__________________________________________________________________________________________
+fpca_est = function(Y, sco_X){
+  sco_X = cbind(1, sco_X)
+  Bhat = ginv(t(sco_X) %*% sco_X) %*% t(sco_X) %*% Y
+  return(Bhat)
+}
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+#__________________________________________________________________________________________
+#______________________________________FPCA_function_______________________________________
+#__________________________________________________________________________________________
+
+fpca = function(Y, X_tr, X_te, npca, nbf, rangeval){
+  np = length(X_tr)
+
+  fpca_X = list()
+  for(fij in 1:np){
+    fpca_X[[fij]] = getPCA(X_tr = X_tr[[fij]], X_te = X_te[[fij]],
+                              nbasis = nbf[fij],  ncomp = npca, rangeval = rangeval[[fij]])
+  }
+  
+  sco_tr = list()
+  sco_te = list()
+  for(i in 1:np){
+    sco_tr[[i]] = fpca_X[[i]]$PCAscore
+    sco_te[[i]] = fpca_X[[i]]$PCAscore_test
+  }
+  
+
+  Bhat = fpca_est(Y = Y, sco_X = do.call(cbind, sco_tr))
+  fits = cbind(1, do.call(cbind, sco_tr)) %*% Bhat
+  preds =  cbind(1, do.call(cbind, sco_te)) %*% Bhat
+  
+  return(list(fits = fits, preds = preds, obj = fpca_X, Bhat = Bhat))
+}
+
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+#__________________________________________________________________________________________
+#__________________________________FPCA_coefficient_estimation_____________________________
+#__________________________________________________________________________________________
+
+getCoef_fpca = function(object, npca){
+  np = length(object$obj)
+  comp_X = list()
+  evb = list()
+  for(i in 1:np){
+    comp_X[[i]] = object$obj[[i]]$PCAcoef
+    evb[[i]] = object$obj[[i]]$evalbase
+  }
+  
+  Bhat = as.matrix(object$Bhat[-1])
+  
+  
+  V = list()
+  for(i in 1:np)
+    V[[i]] = comp_X[[i]]
+
+  Cf = list()
+  km = 1
+  for(i in 1:np){
+    Cf[[i]] = V[[i]] %*% as.matrix(Bhat[km: (km+npca-1),])
+    km = i*npca+1
+  }
+  
+  Bhat = list()
+  for(i in 1:np)
+    Bhat[[i]] = t(Cf[[i]]) %*% t(evb[[i]])
+  
+  return(list(coefs = Bhat))
+}
+
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+#__________________________________________________________________________________________
 #______________________________________Variable_selection__________________________________
 #__________________________________________________________________________________________
-var_select = function(Y, X, nbf=5){
+var_select = function(Y, X, nbf=5, rangeval){
   np = length(X)
   
   dtp = list()
   for(i in 1:np)
-    dtp[[i]] = seq(0, 1, length=ncol(X[[i]]))
+    dtp[[i]] = seq(rangeval[[i]][1], rangeval[[i]][2], length=ncol(X[[i]]))
   
   Bsp_b = list()
   Bsp_f = list()
   for(i in 1:np){
-    Bsp_b[[i]] = create.bspline.basis(c(0,1), nbasis = nbf)
+    Bsp_b[[i]] = create.bspline.basis(c(rangeval[[i]][1], rangeval[[i]][2]), nbasis = nbf[[i]])
     Bsp_f[[i]] = eval.basis(dtp[[i]], Bsp_b[[i]])
   }
   
@@ -226,10 +335,19 @@ var_select = function(Y, X, nbf=5){
   
   Dmat = do.call(cbind, Amat)
   rsm_model = regRSM(y = Y, x = Dmat, B = 1000)
-  empt_mat = rep(NA, nbf*length(X))
+  empt_mat = rep(NA, sum(nbf))
   empt_mat[sort(rsm_model$model)] = sort(rsm_model$model)
-  empt_mat = matrix(empt_mat, nrow = length(X), ncol = nbf, byrow = TRUE)
-  variable_indices = which(rowSums(empt_mat, na.rm = T) > 0)
+  mat_list = list()
+  mat_list[[1]] = empt_mat[1:nbf[1]]
+  ik = nbf[1]
+  for(i in 2:np){
+    mat_list[[i]] = empt_mat[(ik+1):(ik+nbf[i])]
+    ik = ik + nbf[i]
+  }
+  mat_ind = matrix(NA, ncol = 1, nrow = np)
+  for(i in 1:np)
+    mat_ind[i,] = sum(mat_list[[i]], na.rm = TRUE)
+  variable_indices = which(rowSums(mat_ind, na.rm = T) > 0)
   
   return(variable_indices)
 }
@@ -238,60 +356,124 @@ var_select = function(Y, X, nbf=5){
 #__________________________________________________________________________________________
 
 
-#___________________________________________________________________________________________
-#________________________________K-fold_cross-validation____________________________________
-#___________________________________________________________________________________________
+#__________________________________________________________________________________________
+#_________________________________________BIC_function_____________________________________
+#__________________________________________________________________________________________
 
-nfoldCV = function(Y, X, npls_max, nfold, method = c("classical", "robust")){
-  n = dim(X[[1]])[1]
-  fp = length(X)
-  oIndx = 1:n
-  samp_dat = oIndx[sample(n)]
-  folds = cut(seq(1,n), breaks = nfold, labels=FALSE)
+BIC_fun = function(Y, Yfit, h, method = c("classical", "robust")){
+  n = length(Y)
+  arg_bic = (Y - Yfit)^2
   
-  nfMspe = numeric()
+  bic_index = sort.int(arg_bic, decreasing = FALSE,
+                       index.return = TRUE)
+  ntrim = round(0.9 * n)
+  index_trunc = bic_index$ix[1:ntrim]
   
-  for(i in 1:nfold){
-    indx_test = which(folds == i, arr.ind = TRUE)
-    
-    nfY_tr = Y[-indx_test]
-    nfY_te = Y[indx_test]
-    
-    nfX_tr = list()
-    nfX_te = list()
-    
-    for(j in 1:fp){
-      nfX_tr[[j]] = X[[j]][-indx_test,]
-      nfX_te[[j]] = X[[j]][indx_test,]
-    }
-    
-    fold_fpls = fpls_fun(Y = nfY_tr, X_tr = nfX_tr, X_te = nfX_te,
-                         npls = npls_max, method = method)
-    
-    mspe_i = numeric()
-    for(k in 1: npls_max)
-      mspe_i[k] = mean((nfY_te - fold_fpls$all_preds[[k]])^2)
-    
-    nfMspe[i] = which.min(mspe_i)
-    
+  if(method == "classical"){
+    BIC_val = n * log(sum(arg_bic) / n) +
+      h * log(n)
+  }else if(method == "robust"){
+    BIC_val = ntrim * log(sum(arg_bic[index_trunc]) / ntrim) +
+      h * log(ntrim)
   }
-  return(round(mean(nfMspe)))
+  return(BIC_val)
 }
 
-#___________________________________________________________________________________________
-#___________________________________________________________________________________________
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
 
-#___________________________________________________________________________________________
-#__________________________________CPD_and_interval_score___________________________________
-#___________________________________________________________________________________________
 
-interval_score <- function(holdout, lb, ub, alpha){
-  lb_ind = ifelse(holdout < lb, 1, 0)
-  ub_ind = ifelse(holdout > ub, 1, 0)
-  score = (ub - lb) + 2/alpha * ((lb - holdout) * lb_ind + (holdout - ub) * ub_ind)
-  cover = 1 - (length(which(lb_ind == 1)) + length(which(ub_ind == 1)))/length(holdout)
-  cpd = abs(cover - (1 - alpha))
-  return(c(mean(score), cpd))
+#__________________________________________________________________________________________
+#________________________________BIC_function_for_h_(PLS)__________________________________
+#__________________________________________________________________________________________
+
+BIC_nc = function(Y, X, hmax, method = c("classical", "robust")){
+  
+  method = match.arg(method)
+  BIC_model = fpls_fun(Y = Y, X_tr = X, X_te = X,
+                       npls = hmax, method = method)
+  BIC_vec = numeric()
+  for(i in 1:hmax){
+    BIC_vec[i] = BIC_fun(Y = Y, Yfit = BIC_model$all_fits[[i]], h = i, method = method)
+  }
+  
+  BIC_val = min(BIC_vec)
+  hopt = which(BIC_vec == BIC_val)
+  
+  return(list(BIC_val = BIC_val, hopt = hopt))
 }
-#___________________________________________________________________________________________
-#___________________________________________________________________________________________
+
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+#__________________________________________________________________________________________
+#________________________________BIC_function_for_h_(FPCA)_________________________________
+#__________________________________________________________________________________________
+
+BIC_nc_pca = function(Y, X, npca_max, nbf = nbf,
+                      rangeval, method = "classical"){
+  
+  fits_k = list()
+  for(k in 1:npca_max){
+    model_fpca = fpca(Y = Y, X_tr = X, X_te = X,
+                      npca=k, nbf = nbf, rangeval = rangeval)
+    fits_k[[k]] = model_fpca$fits
+  }
+  
+  BIC_vec = numeric()
+  for(i in 1:npca_max){
+    BIC_vec[i] = BIC_fun(Y = Y, Yfit = fits_k[[i]], h = i, method = method)
+  }
+  
+  BIC_val = min(BIC_vec)
+  hopt = which(BIC_vec == BIC_val)
+  
+  return(list(BIC_val = BIC_val, hopt = hopt))
+}
+
+#__________________________________________________________________________________________
+#__________________________________________________________________________________________
+
+
+
+#_____________________________________________________
+#_______________________Trimmed_MSE___________________
+#_____________________________________________________
+tmse = function(Y, fit){
+  
+  ersq = (Y - fit)^2
+  trim_index = sort.int(ersq, decreasing = FALSE,
+                        index.return = TRUE)
+  ntrim = round(0.9 * length(Y))
+  index_trunc = trim_index$ix[1:ntrim]
+  newerr = ersq[index_trunc]
+  
+  trimmed_mse = mean(newerr)
+  return(trimmed_mse)
+}
+
+#_____________________________________________________
+#_____________________________________________________
+
+
+#_____________________________________________________
+#_____________________Trimmed_R^2_____________________
+#_____________________________________________________
+
+trsq = function(Y, fit){
+  ersq = (Y - fit)^2
+  trim_index = sort.int(ersq, decreasing = FALSE,
+                        index.return = TRUE)
+  ntrim = round(0.9 * length(Y))
+  index_trunc = trim_index$ix[1:ntrim]
+  
+  newY = Y[index_trunc]
+  newfir = fit[index_trunc]
+  
+  trimmed_rsq = cor(newY, newfir)^2
+  return(trimmed_rsq)
+}
+
+#_____________________________________________________
+#_____________________________________________________
