24057220b8da068615422baff25a0197e755852d
Ufuk Beyaztas <58028887+UfukBeyaztas@users.noreply.github.com>
2022-01-22 20:53:15 +0300

    Update data_generation.R

--- data_generation.R
+++ data_generation.R
@@ -1,39 +1,7 @@
 library(fda.usc)
 
-data_generation = function(n, j, index, ntrain){
+data_generation = function(n, j, ntrain){
   s = seq(0, 1, length.out = j)
-  
-  gammaexpcov = function(n.point, t){ 
-    Sigma = matrix(0, n.point,n.point)
-    
-    for(i in 1:(n.point-1)){
-      for(j in (i+1):n.point){
-        Sigma[i,j] = exp(-(10*abs(t[j]-t[i]))^2)
-      }
-    }
-    
-    Sigma = (Sigma+t(Sigma))
-    diag(Sigma) = 1
-    return(Sigma)
-  }
-  
-  Sigma.X = gammaexpcov(j, s)
-  
-  eig = eigen(Sigma.X)
-  eigval = eig$values
-  b = cumsum(eigval)/sum(eigval)
-  n.comp.sigma = which(b>0.999999999999)[1]
-  eig0 = eigval[1:n.comp.sigma]
-  sigma.5 =  eig$vectors[,1:n.comp.sigma] %*% sqrt(diag(eig0))
-  range(abs(sigma.5 %*% t(sigma.5) - Sigma.X))
-  
-  V=list()
-  
-  for(i in 1:2){
-    z = matrix(rnorm(n.comp.sigma*n, 0,1), n.comp.sigma, n)
-    V[[i]] = t(sigma.5 %*% z )
-  }
-  
   
   fX = list()
   for(ij in 1:3){
@@ -50,31 +18,26 @@ data_generation = function(n, j, index, ntrain){
     
     fX[[ij]] = Reduce("+", lapply(1:5, function(k){ksi[[k]] %*% t(phi[[k]])}))
   }
-  
-  fX[[4]] = V[[1]]+10
-  fX[[5]] = V[[2]]+10
-  
+
   vBeta = list()
-  vBeta[[1]] = sin(4*pi* s)
-  vBeta[[2]] = exp((s-0.5)^3) * sin(3*pi* s)
-  vBeta[[3]] = cos(4*pi * s)
-  vBeta[[4]] = 0.5*exp(-s^2)
-  vBeta[[5]] = 0.5*exp(-(s+1)^2)
-  
-  for(ij in 1:5){
+  vBeta[[1]] = sin(2*pi* s)
+  vBeta[[2]] = sin(2*pi* s)
+  vBeta[[3]] = cos(2*pi * s)
+
+  for(ij in 1:3){
     fX[[ij]] = fdata(fX[[ij]], argvals = s)
     vBeta[[ij]] = fdata(vBeta[[ij]], argvals = s)
   }
   
   err = rnorm(n, mean=0, sd=1)
   
-  fY = Reduce("+", lapply(1:length(index), function(k){inprod.fdata(fX[[index[k]]], vBeta[[index[k]]])})) 
+  fY = Reduce("+", lapply(1:length(fX), function(k){inprod.fdata(fX[[k]], vBeta[[k]])})) 
   fYe = fY + err
   
   X_train = list()
   X_test = list()
   
-  for(ij in 1:5){
+  for(ij in 1:3){
     X_train[[ij]] = fX[[ij]]$data[1:ntrain,]
     X_test[[ij]] = fX[[ij]]$data[-(1:ntrain),]
   }
